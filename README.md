b6646a45d57e61b53ceddc198af25f5d79c7aa2d
Riccardo Mollo <riccardo.mollo@securegroup.it>
2020-07-17 12:33:10 +0200

    Bug fixes

--- README.md
+++ README.md
@@ -1,1 +1,68 @@
 # DiktStatz
+
+**DiktStatz** is a simple Python script that prints some useful statistical information by reading a given passwords dictionary.
+
+**Usage:**
+```
+usage: diktstatz.py [-h] -d DICTIONARY [-m MAX_LENGTH]
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -d DICTIONARY, --dictionary DICTIONARY
+                        text file containing a lot of juicy passwords
+  -m MAX_LENGTH, --max-length MAX_LENGTH
+                        only passwords with length up to this value will be considered
+```
+
+**Example output:**
+```
+$ ./diktstatz.py -d /tmp/dict.txt
+ _          __            
+| \ o | _|_(_ _|_ _ _|_ _ 
+|_/ | |< |___) |_(_| |_ /_
+
+DiktStatz - Statistics for password dictionaries
+Coded by: Riccardo Mollo
+
+Dictionary file:       /tmp/dict.txt
+Dictionary size:       94461698 bytes
+Max password length:   31 characters
+Avg password length:   9 characters
+Total passwords:       10000000
+Considered passwords:  10000000 (100.00%)
+
++--------+-----------+------------+
+| Length | Passwords | Percentage |
++--------+-----------+------------+
+|      8 |   2734760 |     27.35% |
+|      9 |   1598275 |     15.98% |
+|      6 |   1486136 |     14.86% |
+|     10 |   1336621 |     13.37% |
+|      7 |   1246595 |     12.47% |
+|     11 |    566453 |      5.66% |
+|     12 |    345499 |      3.45% |
+|      5 |    212806 |      2.13% |
+|     13 |    150455 |      1.50% |
+|     14 |    102168 |      1.02% |
+|      4 |     56090 |      0.56% |
+|     15 |     52020 |      0.52% |
+|     16 |     44838 |      0.45% |
+|      3 |     35961 |      0.36% |
+|     17 |      8706 |      0.09% |
+|     18 |      6524 |      0.07% |
+|      2 |      3991 |      0.04% |
+|     19 |      3468 |      0.03% |
+|     20 |      3368 |      0.03% |
+|     21 |      1201 |      0.01% |
+|     22 |      1057 |      0.01% |
+|     23 |       639 |      0.01% |
+|     24 |       592 |      0.01% |
+|     25 |       561 |      0.01% |
+|     30 |       334 |      0.00% |
+|     26 |       295 |      0.00% |
+|     27 |       193 |      0.00% |
+|     28 |       176 |      0.00% |
+|     29 |       114 |      0.00% |
+|      1 |       104 |      0.00% |
++--------+-----------+------------+
+```
--- diktstatz.py
+++ diktstatz.py
@@ -4,7 +4,7 @@
 # DiktStatz.py
 # ------------
 # A simple Python script that prints some useful statistical information by
-# readgin a given passwords dictionary.
+# reading a given passwords dictionary.
 #
 # Coded by: Riccardo Mollo (riccardomollo84@gmail.com)
 #
@@ -37,6 +37,9 @@ def logo():
     print(colored('Coded by: Riccardo Mollo', 'cyan'))
     print()
 
+def error(message):
+    print(colored('ERROR!', 'red', attrs = ['reverse', 'bold']) + ' ' + message)
+
 def percent(n1, n2):
     return '{0:.2f}%'.format((n1 / n2 * 100))
 
@@ -47,7 +50,11 @@ def main(argv):
     args = parser.parse_args()
     dict_file = args.dictionary
     dict_size = os.stat(dict_file).st_size
-    max_length = len(max(open(dict_file), key = len)) if args.max_length is None else int(args.max_length)
+    max_length = len(max(open(dict_file), key = len))
+
+    if args.max_length is not None:
+        if int(args.max_length) > 0 and int(args.max_length) <= max_length:
+            max_length = int(args.max_length) + 1
 
     logo()
 
@@ -79,22 +86,40 @@ def main(argv):
     print('Considered passwords:  ' + str(semit) + ' (' + percent(semit, total) + ')')
     print()
 
-    table = PrettyTable()
-    table.field_names = ['Length', 'Passwords', 'Relative Percentage', 'Total Percentage']
-    table.align['Length'] = 'r'
-    table.align['Passwords'] = 'r'
-    table.align['Relative Percentage'] = 'r'
-    table.align['Total Percentage'] = 'r'
-    table.sortby = 'Passwords'
-    table.reversesort = True
+    if semit == 0:
+        print('No passwords with length less or equal to ' + str(max_length) + ' characters.')
+    else:
+        table = PrettyTable()
 
-    for i in range(max_length):
-        try:
-            table.add_row([i, counters[i], percent(counters[i], semit), percent(counters[i], total)])
-        except KeyError as keyerror:
-            print(keyerror)
+        if args.max_length is None:
+            table.field_names = ['Length', 'Passwords', 'Percentage']
+        else:
+            table.field_names = ['Length', 'Passwords', 'Rel. Percentage', 'Percentage']
 
-    print(table)
+        table.align['Length'] = 'r'
+        table.align['Passwords'] = 'r'
+
+        if args.max_length is None:
+            table.align['Percentage'] = 'r'
+        else:
+            table.align['Rel. Percentage'] = 'r'
+            table.align['Percentage'] = 'r'
+
+        table.sortby = 'Passwords'
+        table.reversesort = True
+
+        for i in range(max_length):
+            if counters[i] > 0:
+                try:
+                    if args.max_length is None:
+                        table.add_row([i, counters[i], percent(counters[i], total)])
+                    else:
+                        table.add_row([i, counters[i], percent(counters[i], semit), percent(counters[i], total)])
+                except KeyError as keyerror:
+                    print(keyerror)
+
+        print(table)
+    print()
 
 if __name__ == '__main__':
     signal.signal(signal.SIGINT, signal_handler)
